2e8d312ed50d2ba779216e4b43cb8b7bf37189c1
katiamiyu <katiamiyu@gmail.com>
2019-11-18 12:32:17 +0100

    fix(new-product-endpoint):
    the endpoint can now perform complete test
    after valid values are entered it creates a product
    
    [Fixes #169779792]

--- server/controller/ProductController.js
+++ server/controller/ProductController.js
@@ -1,43 +1,72 @@
-import conn from '../helpers/conn';
+import conn from "../helpers/conn";
 
 const client = conn();
 client.connect();
 
 class ProductController {
   /**
-       *  Create a new Product
-       *  @param {Object} request
-       *  @param {Object} response
-       *  @return {Object} json
-       */
+   *  Create a new Product
+   *  @param {Object} request
+   *  @param {Object} response
+   *  @return {Object} json
+   */
   static createProduct(request, response) {
     const {
-      productName,
-      shortDescription,
+      prod_name,
+      long_desc,
+      short_desc,
+      discount,
+      coupons,
+      sku_id,
+      price,
+      image_url,
+      available_color,
+      quantity,
+      is_active,
+      last_updated_by
     } = request.body;
 
     const query = {
-      text: 'INSERT INTO products(name, short_desc) VALUES ($1, $2) RETURNING *',
-      values: [productName, shortDescription],
+      text:
+        "INSERT INTO products(prod_name, long_desc, short_desc, discount, coupons, sku_id, price, image_url, available_color, quantity,is_active, last_updated_by) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING *",
+      values: [
+        prod_name,
+        long_desc,
+        short_desc,
+        discount,
+        coupons,
+        sku_id,
+        price,
+        image_url,
+        available_color,
+        quantity,
+        is_active,
+        last_updated_by
+      ]
     };
     ProductController.addProductQuery(response, query);
   }
 
   /**
-       *  Run user addUser query
-       *  @param {Object} request
-       *  @param {Object} response
-       * @param {String} query
-       *  @return {Object} json
-       *
-       */
+   *  Run user addUser query
+   *  @param {Object} request
+   *  @param {Object} response
+   *  @param {String} query
+   *  @return {Object} json
+   *
+   */
   static addProductQuery(response, query) {
-    client.query(query)
-      .then(() => response.status(201).json({
-        status: 201,
-        success: 'Product created successfully'
-      }))
-      .catch(() => response.status(500).json({ status: 500, error: 'Server error' }));
+    client
+      .query(query)
+      .then(() =>
+        response.status(201).json({
+          status: 201,
+          success: "Product created successfully"
+        })
+      )
+      .catch((error) =>
+        response.status(500).json({ status: 500, error: `Server error ${error}` })
+      );
   }
 }
 
